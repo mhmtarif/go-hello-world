fb95ba42274bbf6e9b21384772a08654559fb931
Mehmet Arif ŞEKERCİOĞLU <masekercioglu@anadolu.edu.tr>
2019-01-23 14:22:05 +0300

    port 443

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
+
 import (
-        "fmt"
-        "net/http"
-        "os"
+	"fmt"
+	"net/http"
+	"os"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
-        h, _ := os.Hostname()
-        fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+	h, _ := os.Hostname()
+	fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
 }
 func main() {
-        http.HandleFunc("/", handler)
-        http.ListenAndServe(":8484", nil)
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":443", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
